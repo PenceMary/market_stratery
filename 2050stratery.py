023e80b945e79bdc16a2f1aaf0c4d1b2199b6423
PenceMary <pencemarymany@gmail.com>
2024-07-02 08:07:07 +0800

    1

--- 2050stratery.py
+++ 2050stratery.py
@@ -56,7 +56,7 @@ def simulate_strategy(stock_df, initial_balance=100000):
             balance -= cost
             shares += shares_to_buy
             transactions.append((today.name, 'buy', shares_to_buy, buy_price, balance))
-            print(f"D: {today.name.date()}, T: buy, S: {shares_to_buy}, P: {buy_price:.2f}, B: {balance:.2f}")
+            print(f"{today.name.date()}, B, {shares_to_buy}, {buy_price:.2f}, {balance:.2f}")
         elif shares > 0 and (today['high'] >= (1.1 * buy_price) or today['low'] <= 0.95 * buy_price):
             # 卖出信号（当日最高价达到10%涨幅时卖出）
             if today['high'] >= 1.1 * buy_price:
@@ -66,7 +66,7 @@ def simulate_strategy(stock_df, initial_balance=100000):
             income = shares * sell_price
             balance += income
             transactions.append((today.name, 'sell', shares, sell_price, balance))
-            print(f"D: {today.name.date()}, T: sell, S: {shares}, P: {sell_price:.2f}, B: {balance:.2f}")
+            print(f"{today.name.date()}, S, {shares}, {sell_price:.2f}, {balance:.2f}")
             shares = 0
             buy_price = 0
 
@@ -122,7 +122,8 @@ def main():
 
         # 打印交易信息
         for transaction in transactions:
-            print(f"D: {transaction[0].date()}, T: {transaction[1]}, S: {transaction[2]}, P: {transaction[3]:.2f}, B: {transaction[4]:.2f}")
+            break
+            #print(f"D: {transaction[0].date()}, T: {transaction[1]}, S: {transaction[2]}, P: {transaction[3]:.2f}, B: {transaction[4]:.2f}")
 
         # 打印最终结果
         initial_balance = 100000
