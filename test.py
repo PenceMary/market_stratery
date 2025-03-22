ffd2ef9d55c4ee34e8c4f765886dcab5500c22b4
PenceMary <pencemarymany@gmail.com>
2025-03-22 11:08:27 +0800

    123

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 import akshare as ak
-stockdata = ak.stock_intraday_sina(symbol="sz000001", date="20250304")
+stockdata = ak.stock_intraday_sina(symbol="sz000001", date="20250210")
 print(stockdata)
+#可获取过去15个交易日的成交记录
