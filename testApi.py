1387d8846e3702094c5ffe85e4dee613e3e7cf0b
PenceMary <119424388+PenceMary@users.noreply.github.com>
2025-09-17 08:12:10 +0800

    更新 testApi.py

--- testApi.py
+++ testApi.py
@@ -1,11 +1,13 @@
 import akshare as ak
 
+index_code = "000001"
+start_date = "20250901"
+end_date = "20250915"
 index_data = ak.stock_zh_a_hist(symbol=index_code, period="daily",
-                                      start_date=start_date, end_date=end_date, adjust="")
+                             start_date=start_date,end_date=end_date, adjust="")
 
-        if index_data.empty:
-            print(f"❌ 获取 {index_name} 数据失败，返回空数据")
-        return pd.DataFrame(), "未知指数"
-
+if index_data.empty:
+    print(f"❌ 获取 {index_name} 数据失败，返回空数据")
+else
     print(f"✅ {index_name} 数据获取成功，共 {len(index_data)} 条记录")
-    print(f"   时间范围: {index_data['日期'].min()} 到 {index_data['日期'].max()}")
+    print(f"{index_data}"
